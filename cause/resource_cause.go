8e334fec3d5d9589ddc8bfd02ad0ad6a9a154400
Punit Inani <80308335+puni9869@users.noreply.github.com>
2023-01-16 22:41:35 +0530

    Update resource_cause.go

--- cause/resource_cause.go
+++ cause/resource_cause.go
@@ -62,31 +62,3 @@ func causeDelete(ctx context.Context, d *schema.ResourceData, m interface{}) dia
 	return diags
 }
 
-//
-//func resourceServerCreate(d *schema.ResourceData, m interface{}) error {
-//	uuid_count := d.Get("uuid_count").(string)
-//
-//	d.SetId(uuid_count)
-//
-//	// https://www.uuidtools.com/api/generate/v1/count/uuid_count
-//	resp, err := http.Get("https://www.uuidtools.com/api/generate/v1/count/" + uuid_count)
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//	defer resp.Body.Close()
-
-//	return resourceServerRead(d, m)
-//}
-//
-//func resourceServerRead(d *schema.ResourceData, m interface{}) error {
-//	return nil
-//}
-//
-//func resourceServerUpdate(d *schema.ResourceData, m interface{}) error {
-//	return resourceServerRead(d, m)
-//}
-//
-//func resourceServerDelete(d *schema.ResourceData, m interface{}) error {
-//	d.SetId("")
-//	return nil
-//}
